02dee2aa33114069a3c925e9741e97acb7e86f01
khalid <khalidsmalls@protonmail.com>
2022-08-20 22:54:28 -0400

    model classes and fxml view files are all complete. When you decide to scrap what you're working on and start over, this would be an ideal starting point.

--- src/main/java/smalls/javafxinventorysystem/MainApplication.java
+++ src/main/java/smalls/javafxinventorysystem/MainApplication.java
@@ -2,22 +2,34 @@ package smalls.javafxinventorysystem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import smalls.javafxinventorysystem.controller.MainWindowController;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("mainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            MainWindowController ctrl = new MainWindowController();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("mainWindow.fxml"));
+            loader.setController(ctrl);
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setTitle("JavaFX Inventory System");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         launch();
     }
 }
+
+
