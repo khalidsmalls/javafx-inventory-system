45efa517cff9035fe10f991164b878d21b769589
khalid <khalidsmalls@protonmail.com>
2022-08-23 17:27:54 -0400

    modify part window functions. needs comments, possible light cleaning

--- src/main/java/smalls/javafxinventorysystem/controller/ModifyPartController.java
+++ src/main/java/smalls/javafxinventorysystem/controller/ModifyPartController.java
@@ -1,5 +1,7 @@
 package smalls.javafxinventorysystem.controller;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,7 +11,6 @@ import smalls.javafxinventorysystem.model.InHouse;
 import smalls.javafxinventorysystem.model.Inventory;
 import smalls.javafxinventorysystem.model.Outsourced;
 import smalls.javafxinventorysystem.model.Part;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.function.UnaryOperator;
@@ -36,6 +37,17 @@ public class ModifyPartController implements Initializable {
     private Part part;
     private int partIndex;
     private int partId;
+
+    private InvalidationListener partNameChangeListener;
+    private InvalidationListener partPriceChangeListener;
+    private InvalidationListener partInvChangeListener;
+    private InvalidationListener partMinChangeListener;
+    private InvalidationListener partMaxChangeListener;
+    private boolean partNameModified;
+    private boolean partPriceModified;
+    private boolean partInvModified;
+    private boolean partMinModified;
+    private boolean partMaxModified;
 
     /**
      * the following functional interfaces restrict form input to valid
@@ -95,7 +107,21 @@ public class ModifyPartController implements Initializable {
         autoFillForm(part);
         partIndex = inv.getAllParts().indexOf(part);
         partId = part.getId();
-    }
+
+        partNameModified = false;
+        partPriceModified = false;
+        partInvModified = false;
+        partMinModified = false;
+        partMaxModified = false;
+
+        initInvalidationListeners();
+        partNameTextfield.textProperty().addListener(partNameChangeListener);
+        partPriceTextfield.textProperty().addListener(partPriceChangeListener);
+        partInvTextfield.textProperty().addListener(partInvChangeListener);
+        partMinTextfield.textProperty().addListener(partMinChangeListener);
+        partMaxTextfield.textProperty().addListener(partMaxChangeListener);
+
+    }//END of initialize
 
     public void onInHouseRadioClick() {
         dynamicPartLabel.setText(IN_HOUSE_LABEL);
@@ -109,8 +135,36 @@ public class ModifyPartController implements Initializable {
         dynamicPartTextfield.setTextFormatter(new TextFormatter<String>(stringFilter));
     }
 
-    public void onPartSave() {
-
+    public void onPartSave(ActionEvent event) {
+        if (validateFields()) {
+            if (validateInventory()) {
+                if (partNameModified) {
+                    part.setName(partNameTextfield.getText());
+                }
+                if (partPriceModified) {
+                    part.setPrice(Double.parseDouble(partPriceTextfield.getText()));
+                }
+                if (partInvModified) {
+                    part.setStock(Integer.parseInt(partInvTextfield.getText()));
+                }
+                if (partMinModified) {
+                    part.setMin(Integer.parseInt(partMinTextfield.getText()));
+                }
+                if (partMaxModified) {
+                    part.setMax(Integer.parseInt(partMaxTextfield.getText()));
+                }
+                inv.updatePart(partIndex, part);
+                clearFields();
+                ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
+            } else {
+                //validate inventory returned false
+                new Alert(Alert.AlertType.ERROR, "Inventory must be greater than or equal to Minimum and " +
+                        "less than or equal to Maximum").showAndWait();
+            }
+        } else {
+            //validate fields returned false
+            new Alert(Alert.AlertType.ERROR, "Please enter a value for all fields").showAndWait();
+        }
     }
 
     public void onPartCancel(ActionEvent e) {
@@ -135,6 +189,19 @@ public class ModifyPartController implements Initializable {
         }
     }
 
+    public boolean validateFields() {
+        return !(partNameTextfield.getText().equals("") || partNameTextfield.getText().length() == 0 ||
+                partPriceTextfield.getText().equals("") || partPriceTextfield.getText().length() == 0 ||
+                partInvTextfield.getText().equals("") || partInvTextfield.getText().length() == 0 ||
+                partMaxTextfield.getText().equals("") || partMaxTextfield.getText().length() == 0 ||
+                partMinTextfield.getText().equals("") || partMinTextfield.getText().length() == 0);
+    }
+
+    public boolean validateInventory() {
+        return Integer.parseInt(partMinTextfield.getText()) <= Integer.parseInt(partInvTextfield.getText()) &&
+                (Integer.parseInt(partInvTextfield.getText()) <= Integer.parseInt(partMaxTextfield.getText()));
+    }
+
     private void clearFields() {
         partNameTextfield.clear();
         partPriceTextfield.clear();
@@ -143,115 +210,42 @@ public class ModifyPartController implements Initializable {
         partMaxTextfield.clear();
         dynamicPartTextfield.clear();
     }
+
+    public void initInvalidationListeners() {
+        partNameChangeListener = new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                partNameModified = true;
+            }
+        };
+        partPriceChangeListener = new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                partPriceModified = true;
+            }
+        };
+        partInvChangeListener = new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                partInvModified = true;
+            }
+        };
+        partMinChangeListener = new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                partMinModified = true;
+            }
+        };
+        partMaxChangeListener = new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                partMaxModified = true;
+            }
+        };
+    }
 }
 
-//public class ModifyPartController implements Initializable {
-//    private final String inHouseDynamicLabelText = "Machine ID";
-//    private final String outsourcedDynamicLabelText = "Company Name";
-//    private final Part part;
-//    @FXML
-//    private RadioButton inHouseRadioBtn;
-//
-//    @FXML
-//    private RadioButton outsourcedRadioBtn;
-//
-//    @FXML
-//    private TextField partIdTextfield;
-//
-//    @FXML
-//    private TextField partNameTextfield;
-//
-//    @FXML
-//    private Button modifyPartSaveBtn;
-//
-//    @FXML
-//    private Button modifyPartCancelBtn;
-//
-//    @FXML
-//    private TextField partInvTextfield;
-//
-//    @FXML
-//    private TextField partPriceTextfield;
-//
-//    @FXML
-//    private TextField partMaxTextfield;
-//
-//    @FXML
-//    private TextField partMinTextfield;
-//
-//    @FXML
-//    private TextField dynamicTextfield;
-//
-//    @FXML
-//    private Label dynamicLabel;
-//    private String formLabelText;
-//    @FXML
-//    private Label formLabel;
-//
-//    private String errorMessage;
-//
-//    private ToggleGroup toggleGroup;
-//
-//    private Inventory inv;
-//    private int partIndex;
-//    private int partId;
-//
-//    //functional interfaces for restricting text input to valid characters
-//    private UnaryOperator<TextFormatter.Change> integerFilter = change -> {
-//        String newText = change.getControlNewText();
-//        if (newText.matches("([1-9][0-9]*)?")) {
-//            return change;
-//        }
-//        return null;
-//    };
-//
-//    private UnaryOperator<TextFormatter.Change> doubleFilter = change -> {
-//        String newText = change.getControlNewText();
-//        if (newText.matches("[\\d]*(\\.((\\d{0,2})?))?")) {
-//            return change;
-//        }
-//        return null;
-//    };
-//
-//    private UnaryOperator<TextFormatter.Change> stringFilter = change -> {
-//        String newText = change.getControlNewText();
-//        if (newText.matches("([a-zA-Z\\-]*)")) {
-//            return change;
-//        }
-//        return null;
-//    };
-//
-//    public ModifyPartController(Part p, String formLabelText) {
-//        this.part = p;
-//        this.formLabelText = formLabelText;
-//    }
-//
-//    @Override
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        inv = Inventory.getInstance();
-//        toggleGroup = new ToggleGroup();
-//        toggleGroup.getToggles().addAll(inHouseRadioBtn, outsourcedRadioBtn);
-//        partIdTextfield.setEditable(false);
-//        partIdTextfield.setText(String.valueOf(part.getId()));
-//        formLabel.setText(formLabelText);
-//
-//        partInvTextfield.setTextFormatter(new TextFormatter<Integer>(integerFilter));
-//        partMaxTextfield.setTextFormatter(new TextFormatter<Integer>(integerFilter));
-//        partMinTextfield.setTextFormatter(new TextFormatter<Integer>(integerFilter));
-//        partPriceTextfield.setTextFormatter(new TextFormatter<Double>(doubleFilter));
-//        partNameTextfield.setTextFormatter(new TextFormatter<String>(stringFilter));
-//        autoFillForm(part);
-//        partIndex = inv.getAllParts().indexOf(part);
-//        partId = part.getId();
-//    }
-//
-//    public void onInHouseRadioClick() {
-//        dynamicLabel.setText(inHouseDynamicLabelText);
-//    }
-//
-//    public void onOutsourcedRadioBtnClick() {
-//        dynamicLabel.setText(outsourcedDynamicLabelText);
-//    }
+
 //
 //    public void onSave(ActionEvent e) {
 //        try {
@@ -281,36 +275,5 @@ public class ModifyPartController implements Initializable {
 //            new Alert(Alert.AlertType.ERROR, "Something went wrong").show();
 //        }
 //    }
-//
-//    public void onCancel(ActionEvent e) {
-//        ((Stage) (((Button) e.getSource()).getScene().getWindow())).close();
-//    }
-//
-//
-//    public void autoFillForm(Part p) {
-//        partNameTextfield.setText(p.getName());
-//        partIdTextfield.setText(String.valueOf(p.getId()));
-//        partInvTextfield.setText(String.valueOf(p.getStock()));
-//        partPriceTextfield.setText(String.valueOf(p.getPrice()));
-//        partMaxTextfield.setText(String.valueOf(p.getMax()));
-//        partMinTextfield.setText(String.valueOf(p.getMin()));
-//        if (p instanceof InHouse) {
-//            dynamicTextfield.setText(String.valueOf(((InHouse) p).getMachineId()));
-//            dynamicLabel.setText(inHouseDynamicLabelText);
-//            inHouseRadioBtn.setSelected(true);
-//        } else {
-//            dynamicTextfield.setText(((Outsourced) p).getCompanyName());
-//            dynamicLabel.setText(outsourcedDynamicLabelText);
-//            outsourcedRadioBtn.setSelected(true);
-//        }
-//    }
-//
-//    private void clearFields() {
-//        partNameTextfield.clear();
-//        partPriceTextfield.clear();
-//        partInvTextfield.clear();
-//        partMinTextfield.clear();
-//        partMaxTextfield.clear();
-//        dynamicTextfield.clear();
-//    }
-//}
+
+
