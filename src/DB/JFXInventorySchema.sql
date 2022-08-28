5f72ec8696d7a900d3cdaad2139ca31c31bb7f4e
khalid <khalidsmalls@protonmail.com>
2022-08-27 22:14:45 -0400

    db build script take 2. this one builds :)

--- src/DB/JFXInventorySchema.sql
+++ src/DB/JFXInventorySchema.sql
@@ -1,43 +1,45 @@
-/*
-CREATE DATABASE javafx_inventory_system
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
 
-USE javafx_inventory_system;
-*/
+--CREATE DATABASE javafx_inventory_system
+--CHARACTER SET utf8
+--COLLATE utf8_general_ci;
+
+--USE javafx_inventory_system;
+--CHARSET utf8;
 
 CREATE TABLE part (
     part_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	name VARCHAR(60) NOT NULL,
-	price DECIMAL NOT NULL UNSIGNED,
-	inventory SMALLINT NOT NULL UNSIGNED,
-	min_stock SMALLINT NOT NULL UNSIGNED,
-	max_stock SMALLINT NOT NULL UNSIGNED,
+	price DECIMAL UNSIGNED NOT NULL,
+	inventory SMALLINT UNSIGNED NOT NULL,
+	min_stock SMALLINT UNSIGNED NOT NULL,
+	max_stock SMALLINT UNSIGNED NOT NULL,
 	PRIMARY KEY (part_id)
-)
+);
 
 CREATE TABLE in_house (
 	part_id SMALLINT UNSIGNED NOT NULL,
 	machine_id SMALLINT UNSIGNED NOT NULL,
 	UNIQUE (machine_id),
-	PRIMARY KEY (part_id)  REFERENCES part (part_id)
-)
+	PRIMARY KEY (part_id),
+	FOREIGN KEY (part_id) REFERENCES part (part_id)
+);
 
 CREATE TABLE outsourced (
 	part_id SMALLINT UNSIGNED NOT NULL,
 	company_name VARCHAR(60) NOT NULL,
-	PRIMARY KEY (part_id) REFERENCES part (part_id)
-)
--
+	PRIMARY KEY (part_id) ,
+	FOREIGN KEY (part_id) REFERENCES part (part_id)
+);
+
 CREATE TABLE product (
-    product_id SMALLINT NOT NULL AUTO_INCREMENT,
+    product_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(60) NOT NULL,
-    price DECIMAL NOT NULL UNSIGNED,
-    inventory SMALLINT NOT NULL UNSIGNED,
-    min_stock SMALLINT NOT NULL UNSIGNED,
-    max_stock SMALLINT NOT NULL UNSIGNED,
+    price DECIMAL UNSIGNED NOT NULL,
+    inventory SMALLINT UNSIGNED NOT NULL,
+    min_stock SMALLINT UNSIGNED NOT NULL,
+    max_stock SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY (product_id)
-)
+);
 
 CREATE TABLE product_assoc_parts (
     product_id SMALLINT UNSIGNED NOT NULL,
@@ -45,6 +47,6 @@ CREATE TABLE product_assoc_parts (
     PRIMARY KEY (product_id, part_id),
     FOREIGN KEY (product_id) REFERENCES product (product_id),
     FOREIGN KEY (part_id) REFERENCES part (part_id)
-)
+);
 
 
